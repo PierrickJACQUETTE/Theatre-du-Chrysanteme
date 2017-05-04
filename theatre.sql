fdcd70725624da147b013d498c9b6bd7c66c61f6
jacquette <pierrick.jacquette@etu.univ-paris-diderot.fr>
2017-05-04 17:49:39 +0200

    script teste sur nivose

--- theatre.sql
+++ theatre.sql
@@ -1,22 +1,22 @@
-DROP TABLE IF EXISTS Theatre.ContratDeVentes ;
-DROP TABLE IF EXISTS Theatre.CoutProds ;
-DROP TABLE IF EXISTS Theatre.Tickets ;
-DROP TABLE IF EXISTS Theatre.Representations ;
-DROP TABLE IF EXISTS Theatre.Subventions ;
-DROP TABLE IF EXISTS Theatre.SpectaclesCres ;
-DROP TABLE IF EXISTS Theatre.SpectaclesAchetes ;
-DROP TABLE IF EXISTS Theatre.Spectacles ;
-DROP TABLE IF EXISTS Theatre.Salles ;
-DROP TABLE IF EXISTS Theatre.Organismes ;
+DROP TABLE IF EXISTS ContratDeVentes ;
+DROP TABLE IF EXISTS CoutProds ;
+DROP TABLE IF EXISTS Tickets ;
+DROP TABLE IF EXISTS Representations ;
+DROP TABLE IF EXISTS Subventions ;
+DROP TABLE IF EXISTS SpectaclesCres ;
+DROP TABLE IF EXISTS SpectaclesAchetes ;
+DROP TABLE IF EXISTS Spectacles ;
+DROP TABLE IF EXISTS Salles ;
+DROP TABLE IF EXISTS Organismes ;
 
-DROP SCHEMA IF EXISTS Theatre ;
+DROP TYPE IF EXISTS EnumActions ;
+DROP TYPE IF EXISTS EnumStatus;
 
 CREATE TYPE EnumActions AS ENUM ('creation', 'accueil');
 CREATE TYPE EnumStatus AS ENUM ('reserve', 'achete');
 
-CREATE SCHEMA Theatre;
 
-CREATE TABLE Theatre.Organismes (
+CREATE TABLE Organismes (
   idOrganisme SERIAL PRIMARY KEY,
   nom VARCHAR(50) NOT NULL UNIQUE,
   ville VARCHAR(50) NOT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE Theatre.Organismes (
   pays VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE Theatre.Salles (
+CREATE TABLE Salles (
   idSalle SERIAL PRIMARY KEY,
   capacite INTEGER CHECK (capacite > 0),
   nom VARCHAR(50) NOT NULL,
@@ -33,62 +33,61 @@ CREATE TABLE Theatre.Salles (
   pays VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE Theatre.Spectacles (
+CREATE TABLE Spectacles (
   idSpectacle SERIAL PRIMARY KEY,
   nom VARCHAR(50) UNIQUE NOT NULL,
   tarifNormal INTEGER CHECK (tarifNormal > 0),
   tarifReduit INTEGER CHECK (tarifReduit > 0)
 );
 
-CREATE TABLE Theatre.SpectaclesAchetes (
-  idSpectacle SERIAL PRIMARY KEY REFERENCES Theatre.Spectacles,
+CREATE TABLE SpectaclesAchetes (
+  idSpectacle SERIAL PRIMARY KEY REFERENCES Spectacles,
   prix INTEGER CHECK (prix > 0),
   date DATE DEFAULT CURRENT_DATE,
-  idSalle SERIAL REFERENCES Theatre.Salles
+  idSalle SERIAL REFERENCES Salles
 );
 
-CREATE TABLE Theatre.SpectaclesCres (
-  idSpectacle SERIAL PRIMARY KEY REFERENCES Theatre.Spectacles
+CREATE TABLE SpectaclesCres (
+  idSpectacle SERIAL PRIMARY KEY REFERENCES Spectacles
 );
 
-
-CREATE TABLE Theatre.Subventions (
+CREATE TABLE Subventions (
   id SERIAL PRIMARY KEY,
   action EnumActions NOT NULL,
   date DATE DEFAULT CURRENT_DATE,
   prix INTEGER CHECK (prix > 0),
-  idOrganisme SERIAL REFERENCES Theatre.Organismes,
-  idSpectacle SERIAL REFERENCES Theatre.Spectacles
+  idOrganisme SERIAL REFERENCES Organismes,
+  idSpectacle SERIAL REFERENCES Spectacles
 );
 
-CREATE TABLE Theatre.Representations (
+CREATE TABLE Representations (
   idRepresentation SERIAL PRIMARY KEY,
   date DATE NOT NULL,
   lieu VARCHAR(50) NOT NULL,
   reduction INTEGER CHECK (reduction >= 0),
   condition VARCHAR(50),
   nbPlaces Integer NOT NULL CHECK (nbPlaces > 0),
-  idSpectacle SERIAL REFERENCES Theatre.Spectacles
+  idSpectacle SERIAL REFERENCES Spectacles
 );
 
-CREATE TABLE Theatre.Tickets (
+CREATE TABLE Tickets (
   idTicket SERIAL PRIMARY KEY,
   prix INTEGER CHECK (prix > 0),
   status EnumStatus,
-  idRepresentation SERIAL REFERENCES Theatre.Representations
+  idRepresentation SERIAL REFERENCES Representations
 );
 
-CREATE TABLE Theatre.CoutProds (
+CREATE TABLE CoutProds (
   idCoutProd SERIAL PRIMARY KEY,
   prix INTEGER CHECK (prix > 0),
   date DATE DEFAULT CURRENT_DATE,
-  idSpectacle SERIAL REFERENCES Theatre.SpectaclesCres
+  idSpectacle SERIAL REFERENCES SpectaclesCres
 );
 
-CREATE TABLE Theatre.ContratDeVentes (
+CREATE TABLE ContratDeVentes (
   idContratDeVente SERIAL PRIMARY KEY,
   prix INTEGER CHECK (prix > 0),
   date DATE DEFAULT CURRENT_DATE,
-  idSpectacle SERIAL REFERENCES Theatre.SpectaclesCres,
-  idSalle SERIAL REFERENCES Theatre.Salles
+  idSpectacle SERIAL REFERENCES SpectaclesCres,
+  idSalle SERIAL REFERENCES Salles
 );
